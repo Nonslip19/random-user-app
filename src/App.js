ec38be616207da46b312ad2d5c5f49b833c7344e
Mehmet <mkaymaz93@gmail.com>
2021-12-25 22:24:52 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import './App.css';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
 
-import email from "./assets/email.svg";
-import location from "./assets/location.svg";
-import phone from "./assets/phone.svg";
-
-const api = "https://randomuser.me/api/";
+import Card from './components/Card';
 
 function App() { 
-  const [user, setUser] = useState({});
-  
-  const userData = () => {axios(api).then(res => setUser(res.data.results[0]))
-  .catch((err) => console.log(err));
-  };
 
-  useEffect(() => userData(), []);
- 
-console.log(user);
-
-  return ( 
+  return (  
     <div className="App">
-       <div className='card-container'>
-            <div className="grid-container">
-                <div><img src={user.picture.large} alt=""  className='personImg'/></div>
-                <div>{user.name.title} {user.name.first} {user.name.last}</div>
-                <div><img src={email} alt="email"/></div>
-                <div>{user.email}</div>
-                <div><img src={phone} alt="phone"/></div>  
-                <div>{user.phone}</div>
-                <div><img src={location} alt="location"/></div>   
-                <div>{user.location.state}-{user.location.country}</div>
-            </div> 
-            <div>Age: {user.dob.age}</div> <br />
-            <div>Register Date:{user.registered.date.slice(0,10)}</div>
-        </div>    
-        <button onClick={userData}>Random User</button>
+    
+    <Card/>
+     
     </div>       
-  ); 
-}
+  );
+} 
 export default App; 
--- src/components/Card.js
+++ src/components/Card.js
@@ -10,31 +10,33 @@ const api = "https://randomuser.me/api/";
 
 const Card = () => {
     const [user, setUser] = useState({});
+  
+    useEffect(() => {userData()}, []);
 
-    useEffect(() => {
-        axios(api).then(res => setUser(res.data.results[0]))
-        .catch((err) => console.log(err));
-    }, []);
-
+    const userData = () => { axios(api).then(res => setUser(res.data.results[0]))
+    .catch((err) => console.log(err));
+    };
+   
     console.log(user);
     return (
-        <>
-        mehmet
-        <div className='card-container' key = {user.id.value}>
-            <div className="grid-container">
-                <div><img src={user.picture.large} alt="" style={{width: "100px"}}/></div>
-                <div>{user.name.title} {user.name.first} {user.name.last}</div>
-                <div><img src={email} alt="email"/></div>
-                <div>{user.email}</div>
-                <div><img src={phone} alt="phone"/></div>  
-                <div>{user.phone}</div>
-                <div><img src={location} alt="location"/></div>   
-                <div>{user.location.state}-{user.location.country}</div>
-            </div>
-            <div>Age: {user.registered.age}</div>
-            <div>Register Date:</div>
+<> 
+    <div className='card-container'>
+        <div className="grid-container"> 
+            <div><img src={user.picture.large} alt=""  className='personImg'/></div>
+            <div><h3> {user.name.title} {user.name.first} {user.name.last}</h3></div>
+            <div><img src={email} alt="email"/></div>
+            <div>{user.email}</div>
+            <div><img src={phone} alt="phone"/></div>  
+            <div>{user.phone}</div>
+            <div><img src={location} alt="location"/></div>   
+            <div>{user.location.state}-{user.location.country}</div>
         </div>
-        </>
+            <hr/>
+            <div>Age: {user.dob.age}</div> <br />
+            <div>Register Date: {user.registered.date.slice(0,10)}</div>
+    </div>    
+      <button onClick={userData}>Random User</button>
+</>
         
     )
 }
